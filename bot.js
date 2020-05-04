ed2ab9e523bc98ad3a489c6e4ca5ac8c3c5451c9
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2020-05-04 13:55:41 +0200

    improved guide

--- bot.js
+++ bot.js
@@ -3,29 +3,44 @@ const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 const helpText = `
-  Guide:\n
+Guide:
 
-  Use \`/draw\` command to generate a 256x256 image\n
-  by supplying a JS function which takes \`(x, y)\`\n
-  corrdinates and returns RGB color for that pixel.
+Use /draw command to generate a \`256x256\` image
+by supplying a JS function which takes \`(x, y)\`
+corrdinates and returns RGB color for that pixel.
 
-  Example:\n
-  \`\`\`
-  /draw (x, y) => {
-    ...whatever JS code...
+Example:
 
-    return {
-      r: ...,
-      g: ...,
-      b: ...
-    } 
-  }
-  \`\`\`
+/draw (x, y) => {
+  ...whatever JS code...
+
+  return {
+    r: ...,
+    g: ...,
+    b: ...
+  } 
+}
 `;
+
+bot.start((ctx) => {
+  ctx.reply(`
+    Hello, ImageDrawBot here!\n
+    Use me to generate random images based on a function you supply.\n
+    See \`/help\` for more details.\n\n
+    Have fun :)
+  `);
+});
  
-bot.help((ctx) =>{
+bot.help((ctx) => {
   ctx.reply(helpText)
-})
+});
+
+bot.command('draw', (ctx) => {
+  // const code = ctx?.message?.text;
+
+  // const canvas = createCanvas(200, 200)
+  // const ctx = canvas.getContext('2d')
+});
 
 bot.launch()
  
